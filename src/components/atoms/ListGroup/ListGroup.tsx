4b4024c60472cb75552a7c6a95e1d2be158e0199
Nicolae Tataru <devdfan@gmail.com>
2021-01-29 17:12:31 +0200

    Small fix on list group

--- src/components/atoms/ListGroup/ListGroup.tsx
+++ src/components/atoms/ListGroup/ListGroup.tsx
@@ -10,13 +10,9 @@ export interface ListGroupProps {
   className?: string;
 }
 
-const ListGroup: React.FC<ListGroupProps> & ListGroupComposition = ({ className, children }) => {
-  if (!children) {
-    return null;
-  }
-
-  return <div className={cn(`ebs-list-group`, className)}>{children}</div>;
-};
+const ListGroup: React.FC<ListGroupProps> & ListGroupComposition = ({ className, children }) => (
+  <div className={cn(`ebs-list-group`, className)}>{children}</div>
+);
 
 ListGroup.displayName = 'ListGroup';
 
