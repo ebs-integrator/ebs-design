8ec643512a2f40d1dadd6d721b71bdd992bb8a42
Nicolae Tataru <devdfan@gmail.com>
2021-02-27 14:32:20 +0200

    Fixed position of <Footer /> in <Layout /> & layout story

--- src/components/organisms/Layout/Parts/Content.tsx
+++ src/components/organisms/Layout/Parts/Content.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import cn from 'classnames';
 
-export const Content: React.FC<{ className?: string }> = ({ className, children }) => {
-  return (
-    <div className="ebs-layout__content-wrapper">
-      <div className={cn(`ebs-layout__content`, className)}>{children}</div>
+export const Content: React.FC<{ className?: string }> = ({ className, children }) => 
+  (
+    <div className={cn(`ebs-layout__content-wrapper`, className)}>
+      <div className="ebs-layout__content">{children}</div>
     </div>
   );
-};
