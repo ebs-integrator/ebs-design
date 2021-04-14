b472cb318aad55242f2616baf7842f2532962eb4
Marcel Lefter <marcel.lefter@ebs-integrator.com>
2021-04-14 12:11:54 +0300

    Hotfix date picker types

--- src/components/molecules/DatePicker/types.ts
+++ src/components/molecules/DatePicker/types.ts
@@ -1,23 +1,27 @@
-import type { ReactDatePickerProps } from 'react-datepicker';
+import ReactDatePicker, { ReactDatePickerProps } from 'react-datepicker';
 import { SizeType } from 'types';
 
+type Modify<T, R> = Omit<T, keyof R> & R;
 export type DateType = Date | [Date, Date] | string | /* for selectsRange */ null;
 export type DateValueType = Date | null | undefined;
 
-export interface DatePickerProps extends Partial<ReactDatePickerProps> {
-  // Value is used by form field and value can be other than string
-  value?: any;
-  size?: SizeType;
-  onChange?: (date: DateType, event?: React.SyntheticEvent<HTMLDivElement>) => void;
-}
-export interface RangePickerProps extends DatePickerProps {
+export type DatePickerProps = Modify<
+  ReactDatePickerProps,
+  {
+    size?: SizeType;
+    value?: any; // Value is used by form field and value can be other than string
+    onChange?: (date: DateType, event?: React.SyntheticEvent<HTMLDivElement>) => void;
+  }
+>;
+
+export type RangePickerProps = {
   style?: React.CSSProperties;
   startProps?: DatePickerProps;
   endProps?: DatePickerProps;
-}
+} & DatePickerProps;
 
 export interface DatePickerComposition
-  extends React.ForwardRefExoticComponent<DatePickerProps & React.RefAttributes<HTMLElement>> {
-  Range: React.FC<RangePickerProps>;
-  RangeInput: React.FC<DatePickerProps>;
+  extends React.ForwardRefExoticComponent<DatePickerProps & React.RefAttributes<ReactDatePicker>> {
+  Range: React.ForwardRefExoticComponent<RangePickerProps & React.RefAttributes<ReactDatePicker>>;
+  RangeInput: React.ForwardRefExoticComponent<DatePickerProps & React.RefAttributes<ReactDatePicker>>;
 }
