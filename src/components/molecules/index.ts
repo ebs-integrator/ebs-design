b6ea784b0efa115acd53965a9e3916bd6813f279
Nicolae Tataru <devdfan@gmail.com>
2021-06-18 09:02:07 +0300

    Removed <Calendar />, <DetailsCard />, <InputSelect /> & <SelectDropdown />

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,13 +1,9 @@
 export { Loader } from './Loader/Loader';
-export { Calendar } from './Calendar/Calendar';
 export { DatePicker } from './DatePicker/DatePicker';
 export { InputPhone } from './InputPhone/InputPhone';
 export { InputSearch } from './InputSearch/InputSearch';
 export { Checkbox } from './Checkbox/Checkbox';
 export { CheckboxGroup } from './Checkbox/CheckboxGroup';
-export { InputSelect } from './InputSelect/InputSelect';
-export { SelectDropdown } from './SelectDropdown/SelectDropdown';
-export { SelectDropdownItem } from './SelectDropdown/SelectDropdownItem/SelectDropdownItem';
 export { Stepper } from './Stepper/Stepper';
 export { Textarea } from './Textarea/Textarea';
 export { Upload } from './Upload/Upload';
