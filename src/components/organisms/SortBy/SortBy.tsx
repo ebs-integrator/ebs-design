b33d8d57e4592761232c73e730aaada2c35efb2e
dumitrudanu <dumitru.danu@ebs-integrator.com>
2021-06-24 16:10:41 +0300

    refactor sortBy

--- src/components/organisms/SortBy/SortBy.tsx
+++ src/components/organisms/SortBy/SortBy.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import { Tooltip, Button, Icon } from 'components/atoms';
 import { ButtonSize } from 'components/atoms/Button/Button';
 
-export type Sorts = 'data' | 'relevant' | '';
-
 interface Sort {
   title: React.ReactNode;
   value: string;
@@ -13,7 +11,7 @@ interface Sort {
 export interface SortByProps {
   title?: string;
   sortByTitle?: string;
-  value: Sorts;
+  value?: string;
   options: Sort[];
   size?: ButtonSize;
   onChange: (newValue: string) => void;
@@ -27,41 +25,31 @@ export const SortBy: React.FC<SortByProps> = ({
   value,
   onChange,
 }) => {
-  const [changed, setChanged] = React.useState(false);
-  const [active, setActive] = React.useState('');
-  const [type, setType] = React.useState('asc');
+  const [selected, setSelected] = React.useState(options.find((item) => item.value === value?.replace('-', '')));
+  const [type, setType] = React.useState(value?.charAt(0) === '-' ? 'desc' : 'asc');
 
-  React.useEffect(() => {
-    if (changed) {
-      onChange(`${type === 'desc' ? '-' : ''}${active}`);
+  React.useEffect(()=> {
+    if (selected) {
+      onChange((type === 'asc' ? '' : '-') + selected?.value);
+    } else {
+      onChange('');
     }
-  }, [changed, type, active]);
+  }, [selected, type]);
 
   const onChangeHandler = React.useCallback((newValue) => {
-    setActive((prevActive) => (prevActive !== newValue ? newValue : ''));
-
-    setChanged(true);
+    setSelected((prevActive) => prevActive?.value !== newValue.value ? newValue : undefined);
   }, []);
 
-  const onChangeType = React.useCallback(() => {
-    if (!active.length && options.length) {
-      setActive(options[0].value);
+  const onChangeType = () => {
+    if (!selected && options.length) {
+      setSelected(options[0]);
     }
-
     setType((s) => (s === 'asc' ? 'desc' : 'asc'));
-
-    setChanged(true);
-  }, [options, active]);
-
-  const selected = React.useMemo(
-    () => (options ? options.filter(({ value: val }) => `${type === 'desc' ? '-' : ''}${val}` === value) : []),
-    [value, type],
-  );
+  };
 
   return (
     <div className={`ebs-sort-by__wrapper ebs-sort-${type}`}>
       <Button type="ghost" icon="sort" size={size} onClick={onChangeType} />
-
       <Tooltip
         bodyClass="ebs-sort-by__tooltip"
         trigger="click"
@@ -69,31 +57,27 @@ export const SortBy: React.FC<SortByProps> = ({
         tooltip={
           <>
             <div className="ebs-sort-by__tooltip-title">{title}</div>
-
             <div className="ebs-sort-by__tooltip-items">
-              {options
-                ? options.map(({ value: val, title }) => {
-                    const active = value === `${type === 'desc' ? '-' : ''}${val}`;
-
-                    return (
-                      <div
-                        key={val}
-                        className={`ebs-sort-by__tooltip-item${active ? ' ebs-sort-by__tooltip-item--active' : ''}`}
-                        onClick={() => onChangeHandler(val)}
-                      >
-                        {title}
-
-                        {active && <Icon type="check" model="bold" />}
-                      </div>
-                    );
-                  })
-                : null}
+              {options && options.map((item) => {
+                const active = selected?.value === item.value;
+                return (
+                  <div
+                    key={item.value}
+                    className={`ebs-sort-by__tooltip-item${active ? ' ebs-sort-by__tooltip-item--active' : ''}`}
+                    onClick={() => onChangeHandler(item)}
+                  >
+                    {item.title}
+                    {active && <Icon type="check" model="bold" />}
+                  </div>
+                );
+              })
+              }
             </div>
           </>
         }
       >
         <Button type="ghost" size={size} className={`ebs-sort-by__tooltip-button ebs-sort-${type}`}>
-          {selected.length ? `${sortByTitle} ${selected[0].title}` : title}
+          {selected ? `${sortByTitle} ${selected.title}` : title}
         </Button>
       </Tooltip>
     </div>
