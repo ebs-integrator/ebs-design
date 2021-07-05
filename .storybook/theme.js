9edfc4afba4dc351842d51ab9420b9b8d79c032d
Nicolae Tataru <devdfan@gmail.com>
2021-07-05 11:18:26 +0300

    Customized storybook theme

---
+++ .storybook/theme.js
@@ -0,0 +1,19 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+
+  brandTitle: 'EBS Design',
+  brandUrl: 'https://www.npmjs.com/package/ebs-design',
+  brandImage: 'https://camo.githubusercontent.com/6a76130b93b904e218a2f07f34c5d942f93050dff370e4c48887bd63c35b1708/68747470733a2f2f692e6962622e636f2f674d535768594d2f456c656d656e74732d312e706e67',
+
+  colorPrimary: '#1a39b6',
+  colorSecondary: '#3366ff',
+
+  barSelectedColor: '#3366ff',
+
+  inputBorderRadius: 4,
+  
+  appBorderRadius: 4,
+  appBg: 'white',
+});
