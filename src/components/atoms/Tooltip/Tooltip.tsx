b7720f82495359e676d32f2d412a0febb19ac334
Daniel Suruceanu <daniel.suruceanu@mail.ebs-integrator.com>
2021-08-03 17:54:26 +0300

    Updated react-popper-tooltip version

--- src/components/atoms/Tooltip/Tooltip.tsx
+++ src/components/atoms/Tooltip/Tooltip.tsx
@@ -1,54 +1,96 @@
 import * as React from 'react';
 import cn from 'classnames';
-import TooltipTrigger from 'react-popper-tooltip';
+import { usePopperTooltip } from 'react-popper-tooltip';
+
 import { Icon } from 'components/atoms';
 
-const placements: { [key: string]: string } = {
-  right: 'left',
-  left: 'right',
-  bottom: 'top',
-  top: 'bottom',
-};
+export type TooltipTriggerType = 'click' | 'right-click' | 'hover' | 'focus';
 
-export const Tooltip: React.FC<any> = ({ bodyClass, children, title, tooltip, hideArrow, ...props }) => (
-  <TooltipTrigger
-    {...props}
-    tooltip={({ arrowRef, tooltipRef, getArrowProps, getTooltipProps, placement }) => (
-      <div
-        {...getTooltipProps({
-          ref: tooltipRef,
-          className: `ebs-tooltip__wrapper ebs-tooltip--${placement}`,
-        })}
-      >
-        {!hideArrow && (
-          <Icon
-            type={`arrow-${placements[placement]}`}
-            {...getArrowProps({
-              ref: arrowRef,
-              className: 'ebs-tooltip__arrow',
-              'data-placement': placement,
-            })}
-            model="bold"
-          />
-        )}
+export type TooltipPlacement =
+  | 'auto'
+  | 'auto-start'
+  | 'auto-end'
+  | 'top'
+  | 'top-start'
+  | 'top-end'
+  | 'bottom'
+  | 'bottom-start'
+  | 'bottom-end'
+  | 'right'
+  | 'right-start'
+  | 'right-end'
+  | 'left'
+  | 'left-start'
+  | 'left-end';
 
-        <div className={cn(`ebs-tooltip__body`, bodyClass)}>
-          {title && <div className="ebs-tooltip__body-title">{title}</div>}
+export type TooltipConfig = Partial<{
+  //                             // default value
+  closeOnOutsideClick: boolean; // true
+  closeOnTriggerHidden: boolean; // false
+  defaultVisible: boolean; // false
+  delayHide: number; // 0
+  delayShow: number; // 0
+  followCursor: boolean; // false
+  interactive: boolean;
+  mutationObserverOptions: MutationObserverInit; // { attributes: true, childList: true, subtree: true }
+  offset: [number, number]; // [0, 0]
+  onVisibleChange: (state: boolean) => void;
+  placement: TooltipPlacement;
+  trigger: TooltipTriggerType; // 'hover'
+  visible: boolean;
+}>;
 
-          {tooltip}
-        </div>
-      </div>
-    )}
-  >
-    {({ getTriggerProps, triggerRef }) => (
-      <div
-        {...getTriggerProps({
-          ref: triggerRef,
-          className: 'ebs-tooltip__trigger',
-        })}
-      >
+export interface TooltipProps extends TooltipConfig {
+  bodyClass?: string;
+  title?: React.ReactNode;
+  tooltip?: React.ReactNode;
+  hideArrow?: boolean;
+}
+
+export const Tooltip: React.FC<TooltipProps> = ({
+  bodyClass,
+  children,
+  title,
+  tooltip,
+  hideArrow,
+  ...tooltipConfig
+}) => {
+  const { getArrowProps, getTooltipProps, setTooltipRef, setTriggerRef, visible } = usePopperTooltip({
+    ...tooltipConfig,
+  });
+
+  const { placement } = tooltipConfig;
+
+  return (
+    <div style={{ position: 'relative' }}>
+      <div className="ebs-tooltip__trigger" ref={setTriggerRef}>
         {children}
       </div>
-    )}
-  </TooltipTrigger>
-);
+      {visible && (
+        <div
+          ref={setTooltipRef}
+          {...getTooltipProps({
+            className: `ebs-tooltip__wrapper ebs-tooltip--${placement}`,
+          })}
+        >
+          {!hideArrow && (
+            <div
+              {...getArrowProps({
+                className: 'ebs-tooltip__arrow',
+                'data-placement': placement,
+              })}
+            >
+              <Icon type="arrow-top" />
+            </div>
+          )}
+
+          <div className={cn(`ebs-tooltip__body`, bodyClass)}>
+            {title && <div className="ebs-tooltip__body-title">{title}</div>}
+
+            {tooltip}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
