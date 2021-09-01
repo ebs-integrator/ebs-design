e58fec2e86e66c08e8369a33d5fa314b88abe521
Nicolae Tataru <devdfan@gmail.com>
2021-09-01 15:52:18 +0300

    Fixed loader on <Input />

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -104,16 +104,16 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>(
               className={cn(`ebs-input__prefix`, !loading && props.onClickPrefix ? `clickable` : `not-clickable`)}
               onClick={onClickPrefixHandler}
             >
-              {loading ? <Loader.Spinner size="small" /> : prefix}
+              {loading && !suffix ? <Loader.Spinner size="small" /> : prefix}
             </div>
           ) : null}
 
-          {suffix ? (
+          {loading || suffix ? (
             <div
               className={cn(`ebs-input__suffix`, !loading && props.onClickSuffix ? `clickable` : `not-clickable`)}
               onClick={onClickSuffixHandler}
             >
-              {loading && !prefix ? <Loader.Spinner size="small" /> : suffix}
+              {loading && (!prefix || (prefix && suffix)) ? <Loader.Spinner size="small" /> : suffix}
             </div>
           ) : null}
 
