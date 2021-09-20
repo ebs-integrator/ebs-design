3aa828d60238bcafc05089b90d82eed43267711b
dumitrudanu <dumitru.danu@ebs-integrator.com>
2021-09-20 16:45:03 +0300

    improve actions: * added close action on select option

--- src/components/organisms/Actions/Actions.tsx
+++ src/components/organisms/Actions/Actions.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react';
 import { Tooltip, Icon } from 'components/atoms';
 
+interface ContextProps {
+  onClickItem: (onClick?: () => void) => void;
+}
+
+const ActionContext = React.createContext<ContextProps>({
+  onClickItem: (onClick) => null,
+});
+
 const Item: React.FC<{ onClick?: () => void }> = ({ children, onClick }) => {
+  const { onClickItem } = React.useContext(ActionContext);
+  const handleOnClick = (): void => {
+    onClickItem(onClick);
+  };
   return (
-    <div className="ebs-action__tooltip-item" onClick={onClick}>
+    <div className="ebs-action__tooltip-item" onClick={handleOnClick}>
       {children}
     </div>
   );
@@ -22,17 +34,27 @@ const Actions: React.FC<ActionsProps> = ({
   children,
   ...props
 }) => {
+  const [visible, setVisible] = React.useState(false);
+  const onClickItem = (onClick): void => {
+    if (onClick) {
+      onClick();
+    }
+    setVisible(false);
+  };
   return (
     <div className="ebs-action__wrapper" {...props}>
       <Tooltip
         bodyClass="ebs-action__tooltip"
         trigger="click"
         placement={placement}
+        visible={visible}
+        onVisibleChange={() => setVisible((s) => !s)}
         tooltip={
           <>
             {showTitle && <div className="ebs-action__tooltip-title">{title}</div>}
-
-            <div className="ebs-action__tooltip-items">{children}</div>
+            <ActionContext.Provider value={{ onClickItem }}>
+              <div className="ebs-action__tooltip-items">{children}</div>
+            </ActionContext.Provider>
           </>
         }
       >
