4daad2df63fedac8a1aebe312d18513eb853392f
Nicolae Tataru <devdfan@gmail.com>
2021-11-10 14:41:32 +0200

    Small fix on <TopMenu /> of <Sidebar />

--- src/components/organisms/Sidebar/MenuParts/TopMenu.tsx
+++ src/components/organisms/Sidebar/MenuParts/TopMenu.tsx
@@ -1,17 +1,26 @@
 import * as React from 'react';
+import cn from 'classnames';
 import { Icon } from 'components/atoms';
 import { useLayoutState } from 'components/organisms/Layout/context';
 
 import Item from '../Item';
 
-export const TopMenu: React.FC<{
+export interface TopMenuProps extends React.HtmlHTMLAttributes<HTMLDivElement> {
   showToggle?: boolean;
-  toggleText?: string;
-}> = ({ showToggle = true, toggleText = 'Hide titles', children }) => {
+  toggleText?: React.ReactNode;
+}
+
+export const TopMenu: React.FC<TopMenuProps> = ({
+  className,
+  showToggle = true,
+  toggleText = 'Hide titles',
+  children,
+  ...props
+}) => {
   const { toggled, onSetToggled } = useLayoutState();
 
   return (
-    <div className="ebs-sidebar__top">
+    <div className={cn('ebs-sidebar__top', className)} {...props}>
       {showToggle && onSetToggled !== undefined && (
         <Item
           className="ebs-sidebar__toggler"
