ad36c034bd8d156c9d39239d7ace8bcbfdee3de9
Daniela Vornic <daniela.vornic@gmail.com>
2022-09-28 18:57:24 +0300

    Update deprecated use of --static-dirs in Storybook scripts

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     '../src/components/*.stories.mdx',
     '../src/components/**/*.stories.@(tsx|mdx)',
   ],
+  staticDirs: ['../.storybook/images'],
   core: {
     builder: 'webpack5',
   },
