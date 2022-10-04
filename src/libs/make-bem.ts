84bea58f647cf2c619110dcc93d1b87a9b938e8c
Marcel Lefter <marcel.lefter@ebs-integrator.com>
2022-10-04 16:29:55 +0300

    Merge two bem utils into one

--- src/libs/make-bem.ts
+++ src/libs/make-bem.ts
@@ -1,4 +1,35 @@
-import { createClassWithModifiers } from 'libs';
+/**
+ * @example
+ * const cls = createClassWithModifiers("class",
+ *   {
+ *     "foo": true,
+ *     "bar": false,
+ *     "object": {
+ *        "someProp": 42
+ *     }
+ *   },
+ *   ["array"]
+ * );
+ * cls === "class class--foo class--object class--array"
+ */
+export function createClassWithModifiers(
+  baseClass: string,
+  ...modifierArgs: (Record<string, unknown> | string[] | undefined | null)[]
+): string {
+  const result: string[] = [baseClass];
+
+  modifierArgs.forEach((modifiers) => {
+    if (!modifiers) return;
+
+    if (modifiers instanceof Array) {
+      modifiers.forEach((m) => m && result.push(`${baseClass}--${m}`));
+    } else {
+      Object.entries(modifiers).forEach((entry) => result.push(entry[1] ? baseClass + '--' + entry[0] : ''));
+    }
+  });
+
+  return result.filter((_) => _).join(' ');
+}
 
 /**
  * @example
