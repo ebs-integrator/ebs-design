660db150ce57f65ffcb5b197ed3a8dfa5e357784
Daniela Vornic <daniela.vornic@gmail.com>
2022-10-05 11:58:13 +0300

    Add WithPrefix and IconButton states in Button stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -3,28 +3,59 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import { exportStory } from 'libs';
 import { Icon } from 'components';
+import { Template } from 'components/storybook';
 import { Button } from './Button';
 
 export default {
   title: exportStory('Button', 'form'),
   component: Button,
+  argTypes: {
+    icon: {
+      control: {
+        type: 'select',
+        options: ['home', 'eye', 'search', 'bell', 'info', 'star', 'error'],
+      },
+    },
+  },
 } as ComponentMeta<typeof Button>;
 
 export const Regular: ComponentStory<typeof Button> = (args) => (
-  <div style={{ display: 'flex', justifyContent: 'center' }}>
+  <Template>
     <Button {...args}>Button</Button>
-  </div>
+  </Template>
+);
+
+export const WithPrefix: ComponentStory<typeof Button> = (args) => (
+  <Template>
+    <Button {...args}>Button</Button>
+  </Template>
+);
+
+export const IconButton: ComponentStory<typeof Button> = (args) => (
+  <Template>
+    <Button {...args} />
+  </Template>
 );
 
 Regular.args = {
   type: 'primary',
   size: 'medium',
-  children: 'Example',
-  prefix: <Icon type="home" />,
+  prefix: '',
   icon: '',
   loading: false,
   submit: false,
   disabled: false,
   full: false,
   rounded: false,
+};
+
+WithPrefix.args = {
+  ...Regular.args,
+  prefix: <Icon type="home" />,
+};
+
+IconButton.args = {
+  ...Regular.args,
+  type: 'danger',
+  icon: 'error',
 };
