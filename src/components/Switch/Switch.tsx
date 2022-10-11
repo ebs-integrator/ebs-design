665745a8210e2b03f6e04b56859b979cd204a996
Daniela Vornic <daniela.vornic@gmail.com>
2022-10-11 17:16:05 +0300

    Refactor Switch component

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,27 +1,87 @@
 import * as React from 'react';
 import cn from 'classnames';
-import { makeBEM } from 'libs';
+import { makeBEM, makeid } from 'libs';
+import { Loader } from 'components/Loader/Loader';
 
 const bem = makeBEM('ebs-switch');
 
-export interface SwitchProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'onChange'> {
-  disabled?: boolean;
+type SwitchSize = 'small' | 'medium' | 'large';
+
+export interface SwitchProps extends Omit<Omit<React.HTMLAttributes<HTMLInputElement>, 'onChange'>, 'size'> {
   checked?: boolean;
+  disabled?: boolean;
+  size?: SwitchSize;
+  name?: string;
+  text?: string;
+  error?: boolean;
+  loading?: boolean;
   onChange?: (value: boolean) => void;
 }
 
-export const Switch = ({ className, disabled, checked, onChange, ...props }: SwitchProps) => {
-  const onClickHandler = (): void => (!disabled && onChange !== undefined ? onChange(!checked) : undefined);
+export const Switch = ({
+  className,
+  disabled,
+  name,
+  checked = false,
+  loading = false,
+  size = 'medium',
+  text,
+  error,
+  onChange,
+  ...props
+}: SwitchProps) => {
+  const id = React.useMemo(() => makeid(), []);
+
+  const [isChecked, setIsChecked] = React.useState(checked);
+  const [isFocused, setIsFocused] = React.useState(false);
+  const [isHovered, setIsHovered] = React.useState(false);
+  const [isActive, setIsActive] = React.useState(false);
+
+  const handleChange = ({ target: { checked } }: React.ChangeEvent<HTMLInputElement>) => {
+    if (!disabled && !loading) {
+      setIsChecked(checked);
+      onChange && onChange(checked);
+    }
+  };
+
+  const sliderDataAttributes = {
+    'data-checked': isChecked,
+    ...(disabled && { 'data-disabled': true }),
+    ...(isFocused && { 'data-focused': true }),
+    ...(isHovered && { 'data-hovered': true }),
+    ...(isActive && { 'data-active': true }),
+    ...(loading && { 'data-loading': true }),
+    ...(error && { 'data-error': true }),
+  };
 
   return (
-    <div
-      className={cn(bem(null, { disabled }, [checked ? 'checked' : 'unchecked']), className)}
-      onClick={onClickHandler}
-      {...props}
+    <label
+      className={cn(bem(null, [isChecked ? 'checked' : 'unchecked', size]), className)}
+      data-checked={isChecked}
+      onMouseEnter={() => setIsHovered(!disabled)}
+      onMouseLeave={() => setIsHovered(false)}
+      onMouseDown={() => setIsActive(!disabled)}
+      onMouseUp={() => setIsActive(false)}
+      {...(disabled && { 'data-disabled': true })}
+      {...(loading && { 'data-loading': true })}
     >
-      {checked && <div className={bem('checked-sheet')} />}
+      <input
+        type="checkbox"
+        className={bem('input', [size])}
+        id={id}
+        name={name || id}
+        checked={isChecked}
+        aria-checked={isChecked}
+        onFocus={() => setIsFocused(!disabled)}
+        onBlur={() => setIsFocused(false)}
+        onChange={handleChange}
+        {...props}
+      />
+      <div className={bem('slider', [size])} {...sliderDataAttributes}>
+        {loading && !disabled && <Loader className={bem('loader')} size="small" loading />}
+      </div>
 
-      <div className={bem('dot')} />
-    </div>
+      {text && <span className={bem('text')}>{text}</span>}
+    </label>
   );
 };
